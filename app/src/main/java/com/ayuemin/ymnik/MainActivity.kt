b46693374e519b0d8fe87394d6ef79a63ad1eaf6
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-08 13:44:22 +0300

    Enable edge-to-edge Material You system bars

--- app/src/main/java/com/ayuemin/ymnik/MainActivity.kt
+++ app/src/main/java/com/ayuemin/ymnik/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ayuemin.ymnik
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import com.ayuemin.ymnik.ui.YmnikApp
 
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent { YmnikApp(viewModel) }
     }
 }
