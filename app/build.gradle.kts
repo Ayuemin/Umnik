e88a0e0856cb030f21db8e6b8add3121142822f7
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-11 09:03:55 +0000

    Release Umnik v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-// Umnik v1.1.0
+// Umnik v1.1.1
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -24,8 +24,8 @@ android {
         applicationId = "com.ayuemin.umnik"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "1.1.0"
+        versionCode = 29
+        versionName = "1.1.1"
     }
 
     buildFeatures {
