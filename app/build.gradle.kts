a308387368c5201805c286761420b33cbdc667a7
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-14 20:11:40 +0300

    build: bump Umnik to v1.10.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-// Umnik v1.10.3
+// Umnik v1.10.4
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -24,8 +24,8 @@ android {
         applicationId = "com.ayuemin.umnik"
         minSdk = 26
         targetSdk = 36
-        versionCode = 103
-        versionName = "1.10.3"
+        versionCode = 104
+        versionName = "1.10.4"
     }
 
     buildFeatures {
