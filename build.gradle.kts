e18dd1f7cb2218785c06a4f60845a156623b8069
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-17 16:32:20 +0200

    Migrate build plugins to AGP 9.4 and built-in Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+buildscript {
+    dependencies {
+        // AGP 9 has built-in Kotlin support. Keep KGP aligned with the
+        // Compose compiler plugin and the current stable Kotlin release.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }
