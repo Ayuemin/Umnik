910755b2eded00126e91942251e3bdc4416c2c44
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-17 08:43:33 +0300

    v1.17.5: update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-// Umnik v1.17.4
+// Umnik v1.17.5
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -24,8 +24,8 @@ android {
         applicationId = "com.ayuemin.umnik"
         minSdk = 26
         targetSdk = 36
-        versionCode = 127
-        versionName = "1.17.4"
+        versionCode = 128
+        versionName = "1.17.5"
     }
 
     buildFeatures {
