7f00b45d2400039811b9fa089af572ea46422d08
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-17 16:42:06 +0200

    Use AGP 9 built-in Kotlin without explicit KGP classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-buildscript {
-    dependencies {
-        // AGP 9 has built-in Kotlin support. Keep KGP aligned with the
-        // Compose compiler plugin and the current stable Kotlin release.
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
-    }
-}
-
 plugins {
     id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
