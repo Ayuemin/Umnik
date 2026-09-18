86dee56e753cbf4f4222e3a96f893a88c1411c40
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-18 19:21:20 +0200

    Restrict user profile to ordinary chats only

--- app/src/main/java/com/ayuemin/ymnik/model/UserProfileScopePolicy.kt
+++ app/src/main/java/com/ayuemin/ymnik/model/UserProfileScopePolicy.kt
@@ -4,9 +4,5 @@ internal fun userProfileApplies(
     scope: UserProfileScope,
     inProject: Boolean,
     isAgent: Boolean
-): Boolean = when (scope) {
-    UserProfileScope.OFF -> false
-    UserProfileScope.CHATS -> !inProject && !isAgent
-    UserProfileScope.PROJECTS -> inProject
-    UserProfileScope.EVERYWHERE -> true
-}
+): Boolean =
+    scope == UserProfileScope.CHATS && !inProject && !isAgent
