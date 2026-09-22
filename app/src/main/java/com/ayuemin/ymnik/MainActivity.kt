dcd85acb7634a7f49e76348b3604c10bce8b6fc3
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-22 23:52:43 +0200

    Record previous process exits at startup

--- app/src/main/java/com/ayuemin/ymnik/MainActivity.kt
+++ app/src/main/java/com/ayuemin/ymnik/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import com.ayuemin.ymnik.diagnostics.DiagnosticLog
 import com.ayuemin.ymnik.ui.UmnikV16Root
 
 class MainActivity : ComponentActivity() {
     private val viewModel: ChatViewModel by viewModels { ChatViewModel.Factory(applicationContext) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        DiagnosticLog.installCrashHandler(applicationContext)
+        DiagnosticLog.recordPreviousProcessExit(applicationContext)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent { UmnikV16Root(viewModel) }
